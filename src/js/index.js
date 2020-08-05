d1c269d218e9e83d226506137eeacfd0bdf6f780
pavlokats <paul.flameboy@gmail.com>
2020-08-05 17:33:37 +0300

    LFDU-004: update homepage product items

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import '@/styles/styles.scss';
 import $ from 'jquery';
+
+(function () {
+  let productItem = $('.product-list .item');
+  productItem.height(productItem.width());
+
+  $( window ).resize(function() {
+    productItem.height(productItem.width());
+  });
+})();
